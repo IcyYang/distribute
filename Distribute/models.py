8c49a3b0853fa97e955eb4579542ae9cfede7ae0
IcyYang <mcyang11@126.com>
2018-02-28 16:42:55 +0800

    001

--- Distribute/models.py
+++ Distribute/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-
 # Create your models here.
 
 
@@ -9,18 +8,26 @@ class NRecord(models.Model):
         record_no 叫号的卡号或证件号
         record_date 叫号的时间
         record_status 客户状态：0-等待服务，1-正在服务，2-服务结束
-    '''
 
     status = models.IntegerField(default=0)
     no = models.CharField(max_length=22)
-    arrive_date = models.DateTimeField('Date Record')  # 取号时间
+    arrive_datetime = models.DateTimeField('DateTime Record')  # 取号时间
     start_date = models.DateTimeField('Date Record',default=timezone.now)  # 叫号时间或开始服务时间
     end_date = models.DateTimeField('Date Record',default=timezone.now)  # 结束服务时间
     wait_no = models.CharField(max_length=10) #排队编号
 
     def __str__(self):
-        return  self.arrive_date.strftime("%Y-%m-%d %H:%M:%S") + "  卡号：" + self.no
+        return self.arrive_date.strftime("%Y-%m-%d %H:%M:%S") + "  卡号：" + self.no
+    '''
+    status = models.IntegerField(default=0)
+    no = models.CharField(max_length=22)
+    arrive_date = models.DateTimeField('Date Record')  # 取号时间
+    start_date = models.DateTimeField('Date Record', default=timezone.now)  # 叫号时间或开始服务时间
+    end_date = models.DateTimeField('Date Record', default=timezone.now)  # 结束服务时间
+    wait_no = models.CharField(max_length=10)  # 排队编号
 
+    def __str__(self):
+        return  self.arrive_date.strftime("%Y-%m-%d %H:%M:%S") + "  卡号：" + self.no
 
 class VRecord(models.Model):
     '''
--- Distribute/views.py
+++ Distribute/views.py
@@ -14,11 +14,18 @@ def index(request):
 
 
 def get_new_record():
-    latest_record = VRecord.objects.filter(status__exact=0).order_by('id')[:1]
+    now_time = timezone.now()
+    latest_record = VRecord.objects.filter(status__exact=0,arrive_date__gte=timezone.now() - datetime.timedelta(
+                hours=now_time.hour) - datetime.timedelta(minutes=now_time.minute) - datetime.timedelta(
+                seconds=now_time.second)).order_by('id')[:1]
     if len(latest_record) ==0:
-        latest_record = NRecord.objects.filter(status__exact=0).order_by('id')[:1]
+        latest_record = NRecord.objects.filter(status__exact=0,arrive_date__gte=timezone.now() - datetime.timedelta(
+                hours=now_time.hour) - datetime.timedelta(minutes=now_time.minute) - datetime.timedelta(
+                seconds=now_time.second)).order_by('id')[:1]
         if len(latest_record) > 0:
-            NRecord.objects.filter(wait_no__exact=latest_record[0].wait_no).update(status=1,start_date=timezone.now())
+            NRecord.objects.filter(wait_no__exact=latest_record[0].wait_no,arrive_date__gte=timezone.now() - datetime.timedelta(
+                hours=now_time.hour) - datetime.timedelta(minutes=now_time.minute) - datetime.timedelta(
+                seconds=now_time.second)).update(status=1,start_date=timezone.now())
             return latest_record[0].wait_no
         else:
             return 'noRecord'
@@ -30,26 +37,47 @@ def get_new_record():
 def insert_new_record(custom_no,custom_level):
     date = timezone.now()
     if custom_level == normal:
-        latest_record = NRecord.objects.filter(status__exact=0).order_by('-id')[:1]
-        if (len(latest_record) > 0) & (latest_record[0].arrive_date >= timezone.now() - datetime.timedelta(days=1)):
-                waitno = latest_record[0].wait_no
-                GenerateOrder.normal_number = int(waitno[1:len(waitno)])
+        #latest_record_today = NRecord.objects.filter(arrive_date__gte=timezone.now() - datetime.timedelta(days=1)).order_by('-id')[:1]#今天的最新号码
+        now_time = timezone.now()
+        latest_record_today = NRecord.objects.filter(
+            arrive_date__gte=timezone.now() - datetime.timedelta(
+                hours=now_time.hour) - datetime.timedelta(minutes=now_time.minute) - datetime.timedelta(
+                seconds=now_time.second)).order_by('-id')[:1]
+
+        if len(latest_record_today)>0:
+            waitno = latest_record_today[0].wait_no
+            GenerateOrder.normal_number = int(waitno[1:len(waitno)])
+        else:
+            GenerateOrder.normal_number = 0
 
         waitno = GenerateOrder.getnormalorder(5,'A')
         new_record = NRecord(no=custom_no, wait_no=waitno, arrive_date=date)
         new_record.save()
     elif  custom_level == vip:
-        latest_record = VRecord.objects.filter(status__exact=0).order_by('-id')[:1]
-        if (len(latest_record) > 0) & (latest_record[0].arrive_date >= timezone.now() - datetime.timedelta(days=1)):
-                waitno = latest_record[0].wait_no
-                GenerateOrder.vip_number = int(waitno[1:len(waitno)])
+        now_time = timezone.now()
+        latest_record_today = VRecord.objects.filter(
+            arrive_date__gte=timezone.now() - datetime.timedelta(
+                hours=now_time.hour) - datetime.timedelta(minutes=now_time.minute) - datetime.timedelta(
+                seconds=now_time.second)).order_by('-id')[:1]
+
+        if len(latest_record_today) > 0:
+            waitno = latest_record_today[0].wait_no
+            GenerateOrder.vip_number = int(waitno[1:len(waitno)])
+        else:
+            GenerateOrder.vip_number = 0
+
         waitno = GenerateOrder.getviporder(5,'V')
         new_record = VRecord(no=custom_no, wait_no=waitno, arrive_date=date)
         new_record.save()
 
 
 def set_record_passed(waitno,custom_level):
-    if custom_level == normal:
-        NRecord.objects.filter(wait_no__exact=waitno).update(status=2,end_date=timezone.now())
+    now_time = timezone.now()
+    if custom_level == normal:#将今天的某个排队号码状态设置为已过号
+        NRecord.objects.filter(wait_no__exact=waitno,arrive_date__gte=timezone.now() -  datetime.timedelta(
+                hours=now_time.hour) - datetime.timedelta(minutes=now_time.minute) - datetime.timedelta(
+                seconds=now_time.second)).update(status=2,end_date=timezone.now())
     elif  custom_level == vip:
-        NRecord.objects.filter(wait_no__exact=waitno).update(status=2,end_date=timezone.now())
+        VRecord.objects.filter(wait_no__exact=waitno,arrive_date__gte=timezone.now() - datetime.timedelta(
+                hours=now_time.hour) - datetime.timedelta(minutes=now_time.minute) - datetime.timedelta(
+                seconds=now_time.second)).update(status=2,end_date=timezone.now())
